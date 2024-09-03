ac7eff6681e6d51f298fa69ba801ae62e40fe260
guanghui <995980020@qq.com>
2024-09-03 09:51:43 +0800

    fix: 组件库打包变量找不到问题

--- docs/.vitepress/theme/useComponents.ts
+++ docs/.vitepress/theme/useComponents.ts
@@ -1,12 +1,14 @@
 import Demo from 'vitepress-theme-demoblock/dist/client/components/Demo.vue';
 import Antd from 'ant-design-vue';
 import { GitTalk, TFrame } from './components';
-import hui from 'guanghui-ui';
 
-export function useComponents(app) {
+export async function useComponents(app) {
   app.component('git-talk', GitTalk);
   app.component('t-frame', TFrame);
   app.component('demo', Demo);
   app.use(Antd);
-  app.use(hui);
+  if (!import.meta.env.SSR) {
+    const hui = await import('guanghui-ui');
+    app.use(hui.default);
+  }
 }
--- docs/components/ghui/icon.vue
+++ docs/components/ghui/icon.vue
@@ -1,29 +1,31 @@
 <script lang="ts" setup>
   import { ref } from 'vue';
   import { useClipboard } from '@vueuse/core';
-  import { toast } from 'guanghui-ui';
+  import { message } from 'ant-design-vue';
   import { iconList } from './data';
 
   const source = ref('');
   const copyIconFn = (iconName: string) => {
     source.value = iconName;
     copy();
-    toast({ message: `${source.value}` });
+    message.success(`复制成功😆: ${source.value}`);
   };
   const { text, copy, copied, isSupported } = useClipboard({ source });
 </script>
 
 <template>
   <div>
-    <h-button
-      v-for="iconName in iconList"
-      :key="iconName"
-      @click="copyIconFn(`<h-icon name='${iconName}'></h-icon>`)"
-    >
-      <div class="icon-show-box">
-        <h-icon :name="iconName"></h-icon>
-      </div>
-    </h-button>
+    <client-only>
+      <h-button
+        v-for="iconName in iconList"
+        :key="iconName"
+        @click="copyIconFn(`<h-icon name='${iconName}'></h-icon>`)"
+      >
+        <div class="icon-show-box">
+          <h-icon :name="iconName" size="16px"></h-icon>
+        </div>
+      </h-button>
+    </client-only>
   </div>
 </template>
 
