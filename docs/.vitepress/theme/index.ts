5802031cf504a77ae817e517b0f0fd52cf740872
guanghui <995980020@qq.com>
2024-09-07 15:18:56 +0800

    feat: 主题切换过渡

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,20 +1,22 @@
 // https://vitepress.dev/guide/custom-theme
 import { h } from 'vue';
-import Theme from 'vitepress/theme';
 import { useComponents } from './useComponents';
-import './style.less';
+
+// layout扩展
+import Layout from './Layout.vue';
+
+// 组件库样式
 import 'ant-design-vue/dist/reset.css';
 import 'guanghui-ui/lib/style.css';
 
 export default {
-  extends: Theme,
   Layout: () => {
-    return h(Theme.Layout, null, {
+    return h(Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
     });
   },
-  enhanceApp({ app, router, siteData }) {
+  async enhanceApp({ app }) {
     // ...
-    useComponents(app);
+    await useComponents(app);
   },
 };
